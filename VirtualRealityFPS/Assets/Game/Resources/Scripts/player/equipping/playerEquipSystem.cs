235eb327dc511c3b740af2ee8df2ab8fa3fc03bb
Fergus <ausski101@outlook.com>
2018-11-22 21:57:17 +1000

    Weapon Equip Patch (bugs and efficency still need to be worked on)

--- VirtualRealityFPS/Assets/Game/Resources/Scripts/player/equipping/playerEquipSystem.cs
+++ VirtualRealityFPS/Assets/Game/Resources/Scripts/player/equipping/playerEquipSystem.cs
@@ -7,15 +7,13 @@ public class playerEquipSystem : MonoBehaviour {
     public Transform leftHand;
     public Transform rightHand;
     [Space(10)]
-    public Transform leftHandItem;
-    public Transform rightHandItem;
+    public GameObject leftHandItem;
+    public GameObject rightHandItem;
     [Space(10)]
     public Transform cameraRig;
 	public static bool gunonlefthand;
 	public static bool gunonrighthand;
 	public GameObject[] guns;
-
-
 
 	void Awake()
 	{
@@ -24,13 +22,12 @@ public class playerEquipSystem : MonoBehaviour {
 			gun.GetComponent<VRShoot> ().enabled = false;
 			gun.GetComponent<Lasergun> ().enabled = false;
 			gun.GetComponent<LineRenderer> ().enabled = false;
+        }
 
-		}
-
-	
+	    
 		gunonlefthand = false;
 		gunonrighthand = false;
-	}
+    }
 
     void Update()
     {
@@ -73,78 +70,70 @@ public class playerEquipSystem : MonoBehaviour {
         SteamVR_TrackedController leftHandProperties = leftHandController.GetComponent<SteamVR_TrackedController>();
         SteamVR_TrackedController rightHandProperties = rightHandController.GetComponent<SteamVR_TrackedController>();
 
-
-        Transform leftHandSelection = leftHand.GetComponent<VR_equipDetector>().selectedObject;
-        Transform rightHandSelection = rightHand.GetComponent<VR_equipDetector>().selectedObject;
-
+        GameObject leftHandSelection = leftHand.GetComponent<VR_equipDetector>().selectedObject;
+        GameObject rightHandSelection = rightHand.GetComponent<VR_equipDetector>().selectedObject;
 
 
 
         if (leftHandProperties.gripped)
         {
             leftHandItem = leftHandSelection;
-
-
-
         }
 
         if (rightHandProperties.gripped)
         {
             rightHandItem = rightHandSelection;
-
         }
+        
 
 
         if (leftHandItem)
         {
-            Transform leftItemMount = leftHandSelection.GetComponent<itemHandler>().gripMount;
+            Vector3 leftItemOffset = leftHandItem.transform.position - leftHandItem.GetComponent<itemHandler>().gripCollider.bounds.center;
+            Vector3 leftHandMount = leftHand.GetComponent<VR_equipDetector>().gripCollider.bounds.center;
 
-            leftHandItem.parent = leftHand;
-            leftHandItem.localPosition = leftItemMount.localPosition;
-            leftHandItem.localRotation = leftItemMount.localRotation;
-			gunonlefthand = true;
-			if (leftHand.GetComponent<VR_equipDetector> ().nameofthegun == guns [0].name) 
-			{
-				guns [0].GetComponent<VRShoot> ().enabled = true;
-				guns [0].GetComponent<Lasergun> ().enabled = true;
-				guns [0].GetComponent<LineRenderer> ().enabled = true;
 
-			}
-			if(leftHand.GetComponent<VR_equipDetector> ().nameofthegun == guns [1].name) 
-			{
-				guns [1].GetComponent<VRShoot> ().enabled = true;
-				guns [1].GetComponent<Lasergun> ().enabled = true;
-				guns [1].GetComponent<LineRenderer> ().enabled = true;
+            leftHandItem.transform.position = leftHandMount + leftItemOffset;
+            
+            if (leftHandItem.transform.parent != leftHand)
+            {
+                leftHandItem.transform.rotation = Quaternion.identity;
+                leftHandItem.transform.RotateAround(leftHandItem.transform.position, leftHandItem.transform.right, 54.213f);
+            }
 
-			}
-		
+            leftHandItem.transform.parent = leftHand;
 
+
+            gunonlefthand = true;
+
+            leftHandItem.GetComponent<VRShoot>().enabled = true;
+            leftHandItem.GetComponent<Lasergun>().enabled = true;
+            leftHandItem.GetComponent<LineRenderer>().enabled = true;
+            leftHandItem.GetComponent<Rigidbody>().isKinematic = true;
         }
-
+        
         if (rightHandItem)
         {
-            Transform rightItemMount = rightHandSelection.GetComponent<itemHandler>().gripMount;
-
-            rightHandItem.parent = rightHand;
-            rightHandItem.localPosition = rightItemMount.localPosition;
-			rightHandItem.localRotation = rightItemMount.localRotation;
-			gunonrighthand = true;
-			if (rightHand.GetComponent<VR_equipDetector> ().nameofthegun == guns [0].name) 
-			{
-				guns [0].GetComponent<VRShoot> ().enabled = true;
-				guns [0].GetComponent<Lasergun> ().enabled = true;
-				guns [0].GetComponent<LineRenderer> ().enabled = true;
-
-			}
-			if(rightHand.GetComponent<VR_equipDetector> ().nameofthegun == guns [1].name) 
-			{
-				guns [1].GetComponent<VRShoot> ().enabled = true;
-				guns [1].GetComponent<Lasergun> ().enabled = true;
-				guns [1].GetComponent<LineRenderer> ().enabled = true;
-
-			}
+            Vector3 rightItemOffset = rightHandItem.transform.position - rightHandItem.GetComponent<itemHandler>().gripCollider.bounds.center;
+            Vector3 rightHandMount = rightHand.GetComponent<VR_equipDetector>().gripCollider.bounds.center;
 
 
-		}
+            rightHandItem.transform.position = rightHandMount + rightItemOffset;
+
+            if (rightHandItem.transform.parent != rightHand)
+            {
+                rightHandItem.transform.rotation = Quaternion.identity;
+                rightHandItem.transform.RotateAround(rightHandItem.transform.position, rightHandItem.transform.right, 54.213f);
+            }
+
+            rightHandItem.transform.parent = rightHand;
+
+            gunonrighthand = true;
+
+            rightHandItem.GetComponent<VRShoot>().enabled = true;
+            rightHandItem.GetComponent<Lasergun>().enabled = true;
+            rightHandItem.GetComponent<LineRenderer>().enabled = true;
+            rightHandItem.GetComponent<Rigidbody>().isKinematic = true;
+        }
     }
 }
